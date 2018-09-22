01f3abdcd073cad949d5c8e99d34579972de0b6c
wangchao3 <wangchao3@apusapps.com>
2018-09-22 14:54:43 +0800

    Revert "添加工程文件"
    This reverts commit ee19e649ef57f25fe5183806f5294ab7001349a8.

--- rediswriter/src/main/java/com/alibaba/datax/plugin/writer/rediswriter/Constant.java
+++
@@ -1,5 +0,0 @@
-package com.alibaba.datax.plugin.writer.rediswriter;
-
-public final class Constant {
-    public static final String DEFAULT_DATA_FORMAT = "yyyy-MM-dd HH:mm:ss";
-}
--- rediswriter/src/test/java/com/alibaba/datax/plugin/writer/rediswriter/ProtoBufTest.java
+++
@@ -1,9 +0,0 @@
-package com.alibaba.datax.plugin.writer.rediswriter;
-
-/**
- * @desc:
- * @author: YanMeng
- * @date: 18-8-1
- */
-public class ProtoBufTest {
-}
